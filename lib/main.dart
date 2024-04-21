9ff8a3f50001af73d99c1ccdbd529c356a3dbf7a
Hashir Saud Khan <hashirsaudkhan@gmail.com>
2024-04-21 23:30:26 +0500

    added providers and selectedfavouriteScreen dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:favourite_app_with_provider/providers/selecteditemsprovider.dart';
 import 'package:favourite_app_with_provider/screens/Favourite/favouritescreen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +12,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FavouriteScreen(),
+    return ChangeNotifierProvider(
+      create: (_) => SelectedItemsProvider(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: FavouriteScreen(),
+      ),
     );
   }
 }
--- lib/screens/Favourite/favouritescreen.dart
+++ lib/screens/Favourite/favouritescreen.dart
@@ -1,5 +1,10 @@
+import 'dart:js';
+
+import 'package:favourite_app_with_provider/providers/selecteditemsprovider.dart';
+import 'package:favourite_app_with_provider/screens/Favourite/selectedfavouritescreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
 class FavouriteScreen extends StatefulWidget {
   const FavouriteScreen({super.key});
@@ -11,10 +16,28 @@ class FavouriteScreen extends StatefulWidget {
 class _FavouriteScreenState extends State<FavouriteScreen> {
   @override
   Widget build(BuildContext context) {
+    print('build');
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepPurple[200],
         title: const Text("HSK Favourite App"),
+        actions: [
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (Context) => FavouriteSelectedItemsScreen()));
+            },
+            child: const Icon(
+              Icons.favorite_outlined,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            width: 30,
+          )
+        ],
       ),
       body: Column(
         children: [
@@ -22,7 +45,18 @@ class _FavouriteScreenState extends State<FavouriteScreen> {
             child: ListView.builder(
                 itemCount: 100,
                 itemBuilder: (context, index) {
-                  return Text('Index $index');
+                  return Consumer<SelectedItemsProvider>(
+                      builder: (context, value, child) {
+                    return ListTile(
+                      onTap: () {
+                        value.additeminsellist(index);
+                      },
+                      title: Text("item$index"),
+                      trailing: Icon(value.selecteditems.contains(index)
+                          ? Icons.favorite_outlined
+                          : Icons.favorite_border_outlined),
+                    );
+                  });
                 }),
           )
         ],
