30de7b7655a8faf39f3936a3832f62014697a08b
Hashir Saud Khan <hashirsaudkhan@gmail.com>
2024-04-21 19:29:23 +0500

    created listtile

---
+++ lib/screens/Favourite/favouritescreen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class FavouriteScreen extends StatefulWidget {
+  const FavouriteScreen({super.key});
+
+  @override
+  State<FavouriteScreen> createState() => _FavouriteScreenState();
+}
+
+class _FavouriteScreenState extends State<FavouriteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple[200],
+        title: const Text("HSK Favourite App"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return Text('Index $index');
+                }),
+          )
+        ],
+      ),
+    );
+  }
+}
